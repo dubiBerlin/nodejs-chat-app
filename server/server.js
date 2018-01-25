4f29c0163272229a3c2dfd997e82081bdfc02d77
DubiBerlin <woose@web.de>
2018-01-25 23:24:35 +0100

    Connect and disconnect event added

--- server/server.js
+++ server/server.js
@@ -1,7 +1,6 @@
 const path = require("path");
 const express = require("express");
 const socketIO = require("socket.io");
-
 var http = require('http');
 
 const publicPath = path.join(__dirname, '/../public');
@@ -14,8 +13,18 @@ var io = socketIO(server);
 /* Konfiguration von der express Middleware */
 app.use(express.static(publicPath));
 
-
-
+/*
+functionname: io.on()
+Purpose: registers an event listener  
+param 1: connection event listens to a connection
+param 2: Callback funktion with socket response object*/
+io.on("connection", (socket)=>{
+    console.log("New user connected");
+    
+    socket.on("disconnect", ()=>{
+       console.log("User was disconnected"); 
+    });
+});
 
 
 //__dirname current directory
